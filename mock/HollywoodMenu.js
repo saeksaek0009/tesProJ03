c202571c4353e030291084f7a1f46f1919975dc6
ยุทธพิชัย ไทยทองหลาง <33252420+saeksaek0009@users.noreply.github.com>
2020-04-25 21:38:47 +0700

    Add files via upload

---
+++ mock/HollywoodMenu.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        id: 0,
+        name: 'Big Burger',
+        price: 220,
+        score: 4,
+        rating: 129,
+        image: require('../images/Hollywood/Hollywood001.jpeg'),
+    },
+    {
+        id: 1,
+        name: 'M.C.Burger',
+        price: 150,
+        score: 4,
+        rating: 115,
+        image: require('../images/Hollywood/Hollywood001.jpeg'),
+    },
+    {
+        id: 2,
+        name: 'Cowboy Burger',
+        price: 190,
+        rating: 95,
+        score: 4,
+        rating: 125,
+        image: require('../images/Hollywood/Hollywood001.jpeg'),
+    },
+    {
+        id: 3,
+        name: 'Spicy Burger',
+        price: 120,
+        score: 4,
+        rating: 135,
+        image: require('../images/Hollywood/Hollywood001.jpeg'),
+    },
+]
---
+++ mock/StoreOffers.js
@@ -0,0 +1,123 @@
+export default [
+    {
+        id: 0,
+        name: 'Hollywood Starz Cafe',
+        description: "Cafe,",
+        type: 'burger',
+        rating: 125,
+        score: 3.5,
+        popular: "true",
+        image: require('../images/Hollywood/Hollywood001.jpeg'),
+        menu: [
+            {
+                id: 0,
+                name: 'Big Burger',
+                price: 220,
+                score: 4,
+                rating: 129,
+                image: require('../images/Hollywood/Hollywood001.jpeg'),
+            },
+            {
+                id: 1,
+                name: 'M.C.Burger',
+                price: 150,
+                score: 4,
+                rating: 115,
+                image: require('../images/Hollywood/Hollywood001.jpeg'),
+            },
+            {
+                id: 2,
+                name: 'Cowboy Burger',
+                price: 190,
+                rating: 95,
+                score: 4,
+                rating: 125,
+                image: require('../images/Hollywood/Hollywood001.jpeg'),
+            },
+            {
+                id: 3,
+                name: 'Spicy Burger',
+                price: 120,
+                score: 4,
+                rating: 135,
+                image: require('../images/Hollywood/Hollywood001.jpeg'),
+            },
+        ]
+    },
+    {
+        id: 1,
+        name: 'Manis lan adhem Cafe',
+        description: "Cafe,",
+        type: 'dessert',
+        rating: 124,
+        score: 3.9,
+        popular: "true",
+        image:require('../images/ManislanadhemCafe/ManislanadhemCafe001.jpeg'),
+        menu: [
+            {
+                name:'Cake Roll',
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Pun Pun Happy',
+        description: "Cafe,",
+        type: 'Frappe',
+        rating: 120,
+        score: 3.8,
+        popular: "false",
+        image:require('../images/PunPunHappy/PunPunHappy001.jpg'),
+        menu: [
+            {
+                name:'Pun Frappe',
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'coffe frappe',
+        description: "Cafe,",
+        type: 'Frappe',
+        rating: 120,
+        score: 3.7,
+        popular: "false",
+        image: require('../images/coffefrappe/coffefrappe001.jpg'),
+        menu: [
+            {
+                name:'Mocca Oreo',
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: 'Getty Coff',
+        description: "Cafe,",
+        type: 'Frappe',
+        rating: 120,
+        score: 3.8,
+        popular: "false",
+        image: require('../images/GettyCoff/GettyCoff001.jpg'),
+        menu: [
+            {
+                name:'Love Frapp',
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: 'Yogurt frappe',
+        description: "Cafe,",
+        type: 'Frappe',
+        rating: 120,
+        score: 3.8,
+        popular: "false",
+        image: require('../images/Yogurtfrappe/Yogurtfrappe001.jpg'),
+        menu: [
+            {
+                name:'Stoberry Yoguet',
+            }
+        ]
+    },
+    
+]
